a1d639ba3b118bd3d0735df2e2d37ba682f77f7a
Meshi Yona <meshi@jit.io>
2024-05-01 13:41:50 +0300

    interperter changes

--- interpreter/interperter.py
+++ interpreter/interperter.py
@@ -13,33 +13,16 @@ def execute_step(step_output: StepOutput) -> str:
     return "This is a test message"
 
 
-# this funciton will return this:
-#   {
-# 	"current_objective": "Retrieve and review the Gitleaks output report",
-# 	"required_task": "send_file_content",
-# 	"task_input": {
-# 		"file_name": "tmp/leaks-report.json"
-# 	}
-# }
-#     or:
-# {
-    # 	"objective_status": "completed",
-    # 	"text_output": "Successfully created and verified a Docker image for running Gitleaks. The Docker image scans a mounted '/code' directory and outputs findings to '/tmp/gitleaks-report.json'. The image was tested with a test secret, which Gitleaks successfully identified and logged in the expected JSON format.",
-    # 	"files_map": {
-    # 		"Dockerfile": "The Dockerfile for the container, which sets up Ubuntu 22.04, installs necessary packages, copies the Gitleaks executable, and sets the command to run Gitleaks on the '/code' directory with output directed to '/tmp'.",
-    # 		"code/example.txt": "The test file containing the AWS secret used to validate the Docker image's functionality.",
-    # 		"tmp/gitleaks-report.json": "The output file from Gitleaks containing the findings in JSON format."
-# 	}
-# }
 def execute_feedback_loop(main_prompt: str, prompt: str) -> ObjectiveResult:
     first_prompt = main_prompt + f"\n{prompt}"
     parsed_json, message_history = talk_to_llm(first_prompt, [])
-    system_messages_count = 1
-    status, step_result, message_history = loop(system_messages_count, parsed_json, message_history)
+    status, step_result, message_history = task_loop(parsed_json, message_history)
+    # need to handle the option of step_result=None
     return ObjectiveResult(objective_status=status, text_output=step_result.text_output, files_map=step_result.files_map)
 
 
-def loop(system_messages_count, response_json, message_history):
+def task_loop(response_json, message_history):
+    system_messages_count = 1
     status = response_json.get("objective_status", None)
     while system_messages_count <= MAX_MESSAGES or status is not None:
         new_message = execute_step(StepOutput(**response_json))
